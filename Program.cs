ea6e4cf77dd9bf1d12a777945537ab7dbefac681
IVANNNNN <gravemail@yandex.ru>
2022-09-18 11:05:36 +0300

    Выполнил пункт 1 - запрос данных параметров

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿ 
+Console.WriteLine("Введите число строк (m)");
+if (!int.TryParse(Console.ReadLine()!, out var m))
+{
+     Console.WriteLine("Введите целое число для определения размера массива!");
+}
+
+
+Console.WriteLine("Введите число столбцов (n)");
+if (!int.TryParse(Console.ReadLine()!, out var n))
+{
+     Console.WriteLine("Введите целое число для определения размера массива!");
+}
